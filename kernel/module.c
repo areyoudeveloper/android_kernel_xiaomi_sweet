38e565477f79c17b56afb4d4e5c75b5ce4fef27a
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2022-01-18 17:43:49 +0700

    Fix missing ;

--- kernel/module.c
+++ kernel/module.c
@@ -3740,7 +3740,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	char *after_dashes;
 	//FIXME
 	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
-	flags |= MODULE_INIT_IGNORE_VERMAGIC
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = module_sig_check(info, flags);
 	if (err)
